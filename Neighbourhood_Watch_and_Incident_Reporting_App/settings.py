68c430dba52013d00a41b6dc3ef10b4761f2297a
thanusri2 <thanusri26062007@gmail.com>
2026-09-21 07:12:29 +0530

    Fix Render allowed hosts

--- Neighbourhood_Watch_and_Incident_Reporting_App/settings.py
+++ Neighbourhood_Watch_and_Incident_Reporting_App/settings.py
@@ -20,16 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--dx1tnp0(6z42qjv0#@5p+0r#%b2d(evjwux4_km^i9)h=lqjf'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = []
-
-
-# Application definition
+ALLOWED_HOSTS = [
+    "neighbourhood-watch-and-incident.onrender.com",
+    "localhost",
+    "127.0.0.1",
+]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
