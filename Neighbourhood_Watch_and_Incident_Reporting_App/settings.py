51c5c563b822c712a91316a0faa9d0c3869a9095
thanusri2 <thanusri26062007@gmail.com>
2026-09-21 14:56:35 +0530

    Allow production frontend in CORS

--- Neighbourhood_Watch_and_Incident_Reporting_App/settings.py
+++ Neighbourhood_Watch_and_Incident_Reporting_App/settings.py
@@ -146,4 +146,5 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5174",
     "http://localhost:5175",
     "http://127.0.0.1:5175",
+    "https://neighbourhood-watch-and-incident-ys0x.onrender.com",
 ]
